9b44d4637fe441d1ee5ccb11058d1855e12ab6c5
Reza Morsali <light.reza@gmail.com>
2021-07-30 12:15:24 +0430

    chore: add comments

--- options.go
+++ options.go
@@ -1,13 +1,16 @@
 package spinner
 
+// OptionFunc is a wrapper option func to spinner
 type OptionFunc func(s *Spinner)
 
+// WithOptions passes spinner and runs every option func
 func (s *Spinner) WithOptions(opts ...OptionFunc) {
 	for _, f := range opts {
 		f(s)
 	}
 }
 
+// WithExitOnAbort sets abortOnExit field to spinner
 func WithExitOnAbort(exit bool) OptionFunc {
 	return func(s *Spinner) {
 		s.exitOnAbort = exit
